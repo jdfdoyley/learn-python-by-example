075e91c378047949568d47bfce723b6c0372ebd3
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-16 18:58:26 -0500

    Learn about comments in Python

---
+++ comments.py
@@ -0,0 +1,38 @@
+###############################################################################
+# Day #: 001
+# Date: Nov 16, 2022
+# Topic: Comments
+###############################################################################
+
+# Single-line Comments
+
+# This code prints "Hello, World!"
+print('Hello, World!')
+
+# Indent comments at the same level as the code immediately below them
+
+# Initialize a function
+def hello():
+    # Print "Hello, World!" to the screen
+    print('Hello, World!')
+
+hello()
+
+# Multiline/Block Comments
+# Block comments don't technically exist in Python.
+# However, there are two simple ways to create block comments.
+
+# The First Way
+# Add a # for each line
+# Example
+# Below code will print
+# 'Hello, World!' to the screen
+print('Hello, World!')
+
+# The Second Way
+# Wrapping your comments inside a set of triple quotes """ """
+# This is technically not a comment. It's a bare stromg literal that is not
+# assigned to any variable
+"""Below code will print
+'Hello, World!' to the screen"""
+print('Hello, World!')
