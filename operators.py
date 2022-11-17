a61f0a9fbcb679507958d2c9d18e339128b97e36
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-16 19:50:53 -0500

    Learn about Python operators

---
+++ operators.py
@@ -0,0 +1,89 @@
+###############################################################################
+# Day #: 001
+# Date: Nov 16, 2022
+# Topic: Operators
+###############################################################################
+
+# ARITHEMTIC OPERATORS
+# ====================
+# x = 6
+# y = 2
+
+# addition
+# print(x + y)            # 8
+
+# subtraction
+# print(x - y)            # 4
+
+# multiplication
+# print(x * y)            # 12
+
+# division
+# print(x / y)            # 3.0
+
+# modulus
+# print(x % y)            # 0
+
+# exponentiation
+# print(x ** y)           # 36
+
+# floor division
+# print(x // y)           # 3
+
+# COMPARISON OPERATORS
+# ====================
+# x = 6
+# y = 2
+
+# equal to
+# print(x == y)           # False
+
+# not equal to
+# print(x != y)           # True
+
+# greater than
+# print(x > y)            # True
+
+# less than
+# print(x < y)            # False
+
+# greater than or equal to
+# print(x >= y)            # True
+
+# less than or equal to
+# print(x <= y)            # False
+
+# LOGICAL OPERATORS
+# ====================
+x = 2
+y = -2
+
+# and
+# print(x > 0 and y < 0)          # True
+
+# or
+# print(x > 0 or y < 0)           # True
+
+# not
+# print(not(x > 0 and y < 0))     # False
+
+# IDENTITY OPERATORS
+# ====================
+# x = [1, 2, 3]
+# y = [1, 2, 3]
+
+# is
+# print(x is y)           # False
+
+# is not
+# print(x is not y)       # True
+
+# MEMBERSHIP OPERATORS
+# ====================
+colors = ['red', 'green', 'blue']
+
+# in
+print('red' in colors)          # True
+
+# not in
+print('yellow' not in colors)   # True
