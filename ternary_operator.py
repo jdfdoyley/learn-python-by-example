1233f6c6debc12601303e3b182d35b525880e53a
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:42:34 -0500

    Learn about conditional expressions

--- ternary_operator.py
+++ ternary_operator.py
@@ -5,3 +5,8 @@
 # Conditional Expressions (ternary operator)
 # Conditional expression (sometimes referred to as ‘ternary operator’) allows
 # us to select one of two statements depending on the specified condition.
+x, y = 7, 5
+
+print('x is greater') if x > y else print('y is greater')
+
+# Prints: x is greater
