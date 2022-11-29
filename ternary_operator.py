26e198d3e3b0e66901d0295dfecfe2e98fff5fb2
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:42:20 -0500

    Learn about conditional expressions

---
+++ ternary_operator.py
@@ -0,0 +1,7 @@
+# Name: Jason D'Oyley
+# Date: Nov 29, 2022
+# Day #: 001
+
+# Conditional Expressions (ternary operator)
+# Conditional expression (sometimes referred to as ‘ternary operator’) allows
+# us to select one of two statements depending on the specified condition.
