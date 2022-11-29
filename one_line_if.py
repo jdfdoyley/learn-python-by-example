7e08fd51dc2a225d73e68b53235891a1dc303c4a
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:38:51 -0500

    Learn about one line if statements

---
+++ one_line_if.py
@@ -0,0 +1,20 @@
+# Name: Jason D'Oyley
+# Date: Nov 29, 2022
+# Day #: 001
+
+# One Line if Statement
+# Python allows us to write an entire if statement on one line.
+x, y = 7, 5
+if x > y: print('x is greater')
+
+# Prints: x is greater
+
+# You can even keep several lines of code on just one line, simply by
+# separating them with a semicolon ;
+x, y = 7, 5
+if x > y: print('x is greater'); print('y is smaller'); print('x and y are not equal')
+
+# Prints:
+# x is greater
+# y is smaller
+# x and y are not equal
