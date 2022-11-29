845d10dc88c30288d5b568a2e3837de06632e7b9
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:21:58 -0500

    Learn about elif statement

---
+++ elif.py
@@ -0,0 +1,17 @@
+# Name: Jason D'Oyley
+# Date: Nov 29, 2022
+# Day #: 001
+
+# The elif (else if) Statement
+# Use elif statement to specify a new condition to test, if the first
+# condition is false.
+x, y = 5, 5
+if x > y:
+    print('x is greater')
+elif x < y:
+    print('y is greater')
+else:
+    print('x and y are equal')
+
+# Prints
+# x and y are equal
