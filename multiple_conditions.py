a2bf44ce7ab584d5ae1f6807c89778a0b04d1e58
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:29:40 -0500

    Learn multiple conditions with and expression

---
+++ multiple_conditions.py
@@ -0,0 +1,14 @@
+# Name: Jason D'Oyley
+# Date: Nov 29, 2022
+# Day #: 001
+
+# Multiple Conditions
+# To join two or more conditions into a single if statement, use logical
+# operators viz. and, or and not.
+
+# and expression is True, if all the conditions are true.
+x, y, z = 7, 4, 2
+if x > y and x > z:
+    print('x is greater')
+
+# Prints: x is greater
