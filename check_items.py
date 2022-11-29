d737b0a8c54cf9041ccb832595e8b5b77944693e
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:48:01 -0500

    Learn to check if item present in sequences

---
+++ check_items.py
@@ -0,0 +1,27 @@
+# Name: Jason D'Oyley
+# Date: Nov 29, 2022
+# Day #: 001
+
+# Check If Item Present in a Sequence
+# The in operator is used to check if a value is present in a sequence (list,
+# tuple, string etc.).
+
+# list
+colors_list = ['red', 'green', 'blue']
+if 'red' in colors_list:
+    print('yes')
+
+# Prints: yes
+
+colors_tuple = ['red', 'green', 'blue']
+if 'red' in colors_tuple:
+    print('yes')
+
+# Prints: yes
+
+# string
+greet = 'Hello, World'
+if 'Hello' in greet:
+    print('yes')
+
+# Prints: yes
