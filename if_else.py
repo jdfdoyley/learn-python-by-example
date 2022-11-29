5704b959e483be4a35df02319857155ce18b0c54
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:18:08 -0500

    Learn about if else statement

---
+++ if_else.py
@@ -0,0 +1,16 @@
+# Name: Jason D'Oyley
+# Date: Nov 29, 2022
+# Day #: 001
+
+# The else Statement
+# Use else statement to execute a block of Python code, if the condition is
+# false.
+x, y = 7, 5
+
+if x < y:
+    print("y is greater")
+else:
+    print("x is greater")
+
+# Prints
+# x is greater
