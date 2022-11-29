60003e2b9304009bf8da046500dc8f98305772ea
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 18:31:22 -0500

    Learn how to pass multiple argument to f(x)

--- Functions/pass_argument.py
+++ Functions/pass_argument.py
@@ -13,3 +13,11 @@ hello("Rhage")
 
 hello("Wrath")
 # Prints: Hello, Wrath
+
+# Pass two arguments
+def person_info(name, job):
+    print(name, "is a", job)
+
+
+person_info("Rhage", "Member of the Black Dagger Brotherhood")
+# Prints: Rhage is a Member of the Black Dagger Brotherhood
