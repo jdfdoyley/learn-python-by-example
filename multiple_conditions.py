869381649361f29dc30e3a6fb581a0f9ba106bf3
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:31:31 -0500

    Learn multiple conditions with or expression

--- multiple_conditions.py
+++ multiple_conditions.py
@@ -7,8 +7,15 @@
 # operators viz. and, or and not.
 
 # and expression is True, if all the conditions are true.
-x, y, z = 7, 4, 2
-if x > y and x > z:
-    print('x is greater')
+# x, y, z = 7, 4, 2
+# if x > y and x > z:
+#     print('x is greater')
 
 # Prints: x is greater
+
+# or expression is True, if at least one of the conditions is True.
+x, y, z = 7, 4, 9
+if x > y or x > z:
+    print('x is greater than y or z')
+
+# Prints: x is greater than y or z
