da0f2dae72136d115fe206c21a22f437528704ab
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 07:10:27 -0500

    Learn basic if statement

---
+++ basic_if.py
@@ -0,0 +1,23 @@
+x, y = 7, 5
+
+if x > y:
+    print('x is greater')
+
+# Prints: x is greater
+
+# any non-zero value
+if -3:
+    print('True')
+# Prints: True
+
+# mathematical expression
+x, y = 7, 5
+if x + y:
+    print('True')
+# Prints: True
+
+# nonempty container
+L = ['red', 'green']
+if L:
+    print('True')
+# Prints: True
