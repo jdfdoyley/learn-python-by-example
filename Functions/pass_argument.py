632af5bb729ed18c979656e30ceb757cef31b557
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-29 18:26:51 -0500

    Learn how to pass arguments to a f(x)

---
+++ Functions/pass_argument.py
@@ -0,0 +1,15 @@
+# Name: Jason D'Oyley
+# Date: Nov 29, 2022
+# Day #: 001
+# Topic: Pass Argument
+
+# Pass single argument to a function
+def hello(name: str):
+    print("Hello,", name)
+
+
+hello("Rhage")
+# Prints: Hello, Rhage
+
+hello("Wrath")
+# Prints: Hello, Wrath
